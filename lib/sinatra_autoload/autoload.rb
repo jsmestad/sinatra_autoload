97de2ee78ed47ad14d73969392fd6ca9e428a7db
Gabe Varela <gvarela@gmail.com>
2009-10-30 17:05:16 -0600

    actually autoload, thanks for copying the right code in jstad ;P

--- lib/sinatra_autoload/autoload.rb
+++ lib/sinatra_autoload/autoload.rb
@@ -15,7 +15,7 @@ module SinatraAutoload
     return unless args
     args.each do |file_path|
       Dir[ File.join( @root_path, file_path, '/**/*.rb') ].each do |file|
-        require file
+        autoload File.basename(file, '.rb').classify.to_sym, file
       end
     end
   end
